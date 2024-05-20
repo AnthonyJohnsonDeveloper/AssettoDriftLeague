4a6203fe2c9e8bd39a14271f4be939621dcfcff0
Anthony Johnson <anthonyjohnson5016@gmail.com>
2024-05-20 09:38:58 -0400

    new changes to fix the chat error

--- app/(tabs)/Chat/_layout.tsx
+++ app/(tabs)/Chat/_layout.tsx
@@ -8,30 +8,6 @@ const API_KEY = "4qs628f6q3nc"
 const client = StreamChat.getInstance(API_KEY);
 
 export default function ChatLoayout() {
-  useEffect(() => {
-    const connectUser = async () => {
-      await client.connectUser(
-        {
-          id: "dj",
-          name: "Dj",
-          image: "https://i.imgur.com/fR9Jz14",
-        },
-        client.devToken("dj")
-      );
-
-      const channel = client.channel("Livestream", "public", {
-        name: "Public",
-      });
-      await channel.create();
-    };
-
-    connectUser();
-
-    return () => {
-      client.disconnectUser();
-    };
-  }, []);
-
   return (
     <Stack>
       
--- app/(tabs)/Chat/index.tsx
+++ app/(tabs)/Chat/index.tsx
@@ -1,22 +1,11 @@
 import React from "react";
 import { StreamChat } from "stream-chat";
-import { Chat, Channel, MessageList, DefaultStreamChatGenerics, OverlayProvider } from 'stream-chat-expo';
-import ChannelComponent from "./ChannelComponent";
+import { Chat, Channel, MessageList, OverlayProvider } from 'stream-chat-expo';
 const API_KEY = "4qs628f6q3nc";
 const client=StreamChat.getInstance(API_KEY);
 
 const ChatScreen = () => {
-  return (
-  <OverlayProvider> 
-    <Chat client={client}>
-      <Channel
-        channel={{ type: "messaging", id: "general" }}
-      >
-        <MessageList />
-      </Channel>
-    </Chat>
-    </OverlayProvider>   
-  );
+  return 
 };
 
 export default ChatScreen;
